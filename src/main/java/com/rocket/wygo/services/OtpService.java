da004d5bb2ac8e9a8bac1baf20f3ec980ad09753
Huỳnh Nam Duy <119779697+Dahlia3003@users.noreply.github.com>
2024-05-13 15:09:11 +0700

    update database

--- src/main/java/com/rocket/wygo/services/OtpService.java
+++ src/main/java/com/rocket/wygo/services/OtpService.java
@@ -1,8 +1,11 @@
 package com.rocket.wygo.services;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -26,12 +29,37 @@ public class OtpService {
 
     // Gửi mã OTP đến email của người dùng
     public void sendOTP(String email, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject("Xác thực OTP");
-        message.setText("Mã OTP của bạn là: " + otp);
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        try {
+            helper.setTo(email);
+            helper.setSubject("Xác thực OTP");
+            String content = "<html>" +
+                    "<head>" +
+                    "<style>" +
+                    "body {font-family: Arial, sans-serif; background-color: #f4f4f4;}" +
+                    ".container {background-color: #fff; padding: 20px; border-radius: 15px; margin: 10px auto; max-width: 500px;}" +
+                    ".otp {color: #3090C7; font-size: 20px; font-weight: bold;}" +
+                    "h2 {color: #444;}" +
+                    "p {color: #666;}" +
+                    "</style>" +
+                    "</head>" +
+                    "<body>" +
+                    "<div class='container'>" +
+                    "<h2>Xác thực OTP</h2>" +
+                    "<p>Mã OTP của bạn là: <span class='otp'>" + otp + "</span></p>" +
+                    "</div>" +
+                    "</body>" +
+                    "</html>";
+            helper.setText(content, true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
         mailSender.send(message);
     }
+
 
     // Lưu mã OTP vào map (hoặc cơ sở dữ liệu)
     public void saveOTP(String email, String otp) {
